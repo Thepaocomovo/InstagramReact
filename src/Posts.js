f8582dce56c1e5fb626a7bf35c090186f70649e8
wesley <wesleysantos20012001@gmail.com>
2022-07-18 00:44:51 -0300

    like bug fixed; like numbers; hover setinha; icons:hover.

--- src/Posts.js
+++ src/Posts.js
@@ -1,33 +1,53 @@
 import React from "react";
 
-const array = [];
-
-function ArrayPosts(props) {
-    array.push(props);
-    return ""
-}
 
 
+const arrayPosts = [
+    {
+        usuarioIMG: "./assets/img/meowed.svg",
+        usuario: "meowed",
+        conteudoIMG: "./assets/img/gato-telefone.svg",
+        curtidoIMG: "./assets/img/respondeai.svg",
+        curtidoUser: "respondeai",
+        curtidoQTD: 101523
+    },
+    {
+        usuarioIMG: "/assets/img/barked.svg",
+        usuario: "barked",
+        conteudoIMG: "./assets/img/dog.svg",
+        curtidoIMG: "./assets/img/adorable_animals.svg",
+        curtidoUser: "adorable_animals",
+        curtidoQTD: 99159
+    }
+]
 
-function GeneratePost() {
+
+
+
+
+function GeneratePost(props) {
+    let curtidoQTD = (props.curtidoQTD)
     const [icon, setIcon] = React.useState("heart-outline");
-    const [iconColor, setIconColor] = React.useState({color: "#202124"});
-    function Clique(){
-        if(icon === "heart-outline"){
+    const [iconColor, setIconColor] = React.useState({ color: "#202124" });
+    const [number, setNumber] = React.useState(curtidoQTD.toFixed(0).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'));
+    function Clique() {
+        if (icon === "heart-outline") {
             setIcon("heart");
-            setIconColor({color: "red"});
-            // alert(iconClass);
-        } else{
+            setIconColor({ color: "red" });
+            setNumber((curtidoQTD + 1).toFixed(0).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'));
+        } else {
             setIcon("heart-outline");
-            setIconColor({color: "#202124"});
+            setIconColor({ color: "#202124" });
+            setNumber((curtidoQTD).toFixed(0).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'));
         }
     }
-    
-    const posts = array.map((post) => <div className="post">
+
+
+    return <div className="post">
         <div className="topo">
             <div className="usuario" >
-                <img src={post.usuarioIMG} alt=""  />
-                {post.usuario}
+                <img src={props.usuarioIMG} alt="" />
+                {props.usuario}
             </div>
             <div className="acoes">
                 <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -35,13 +55,13 @@ function GeneratePost() {
         </div>
 
         <div className="conteudo">
-            <img src={post.conteudoIMG} alt="" onDoubleClick={()=>(Clique())}/>
+            <img src={props.conteudoIMG} alt="" onDoubleClick={() => (Clique())} />
         </div>
 
         <div className="fundo">
             <div className="acoes">
                 <div>
-                    <ion-icon style={iconColor}  name={icon} onClick={()=>(Clique())}></ion-icon>
+                    <ion-icon style={iconColor} name={icon} onClick={() => (Clique())}></ion-icon>
                     <ion-icon name="chatbubble-outline"></ion-icon>
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
@@ -51,23 +71,32 @@ function GeneratePost() {
             </div>
 
             <div className="curtidas">
-                <img src={post.curtidoIMG} alt="" />
+                <img src={props.curtidoIMG} alt="" />
                 <div className="texto">
-                    Curtido por <strong>{post.curtidoUser}</strong> e <strong>outras {post.curtidoQTD} pessoas</strong>
+                    Curtido por <strong>{props.curtidoUser}</strong> e <strong>outras {number} pessoas</strong>
                 </div>
             </div>
         </div>
-    </div>)
-    return posts
+    </div>
 }
 
 
 function Posts() {
     return <div className="posts">
-        <ArrayPosts usuarioIMG="./assets/img/meowed.svg" usuario="meowed" conteudoIMG="./assets/img/gato-telefone.svg" curtidoIMG="./assets/img/respondeai.svg" curtidoUser="respondeai" curtidoQTD="101.523" />
-        <ArrayPosts usuarioIMG="/assets/img/barked.svg" usuario="barked" conteudoIMG="./assets/img/dog.svg" curtidoIMG="./assets/img/adorable_animals.svg" curtidoUser="adorable_animals" curtidoQTD="99.159" />
-        <GeneratePost/>
+
+    <ArrayPosts/>
+
     </div>
 }
 
 export default Posts;
+
+function ArrayPosts() {
+    const post = arrayPosts.map((post) => < GeneratePost usuarioIMG={post.usuarioIMG}
+        usuario={post.usuario}
+        conteudoIMG={post.conteudoIMG}
+        curtidoIMG={post.curtidoIMG}
+        curtidoUser={post.curtidoUser}
+        curtidoQTD={post.curtidoQTD} />)
+    return post
+}
